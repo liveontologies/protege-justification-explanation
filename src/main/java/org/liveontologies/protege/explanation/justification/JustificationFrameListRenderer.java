b8507351e27a474f975ce0215e09e696fea38957
Alexander Stupnikov <saarus72@gmail.com>
2017-06-21 14:32:20 +0200

    Remove icons for axioms

--- src/main/java/org/liveontologies/protege/explanation/justification/JustificationFrameListRenderer.java
+++ src/main/java/org/liveontologies/protege/explanation/justification/JustificationFrameListRenderer.java
@@ -22,8 +22,11 @@ package org.liveontologies.protege.explanation.justification;
  * #L%
  */
 
+import javax.swing.JList;
+
 import org.protege.editor.owl.OWLEditorKit;
 import org.protege.editor.owl.ui.framelist.OWLFrameListRenderer;
+import org.semanticweb.owlapi.model.OWLObject;
 
 /**
  * Author: Matthew Horridge Stanford University Bio-Medical Informatics Research
@@ -36,5 +39,11 @@ public class JustificationFrameListRenderer extends OWLFrameListRenderer {
 		super(owlEditorKit);
 		setHighlightUnsatisfiableClasses(false);
 		setHighlightUnsatisfiableProperties(false);
+	}
+
+	@Override
+	protected OWLObject getIconObject(JList list, Object value, int index,
+			boolean isSelected, boolean cellHasFocus) {
+		return null;
 	}
 }
